72ee8c92e94f17574b28ee26cb1133dfcb34b72d
natcom <nathankurz91@gmail.com>
2018-06-08 14:40:04 -0400

    remove unused links commit

---
+++ php/signup.php
@@ -0,0 +1,8 @@
+<?php
+  $username = $_POST("username");
+  $email = $_POST("email");
+  $password = $_POST("password");
+  echo "Hello! Your username is: $password <br>";
+  echo "Your email is: $email <br>";
+  echo "Your password is $password <br>";
+ ?>
