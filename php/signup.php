82859feb826dfacfb464627cf6cddc01265785a6
Param Ri <paramri@gatech.edu>
2018-06-11 15:10:47 -0400

    fixed url redirect problem. need to fix text margins

--- php/signup.php
+++ php/signup.php
@@ -1,8 +1,22 @@
+<html>
+<body>
 <?php
-  $username = $_POST("username");
-  $email = $_POST("email");
-  $password = $_POST("password");
-  echo "Hello! Your username is: $password <br>";
+  $username = $_POST['username'];
+  $email = $_POST['email'];
+  $password = $_POST['password'];
+  $confirm_password = $_POST['confirm_password'];
+  echo "Hello! Your username is: $username <br>";
   echo "Your email is: $email <br>";
-  echo "Your password is $password <br>";
+  if ($password == $confirm_password) echo "passwords match <br>";
+  if ($password != $confirm_password) echo "passwords don't match <br>";
+  
+  //$mysqli = new mysqli("localhost", "$username", "$password", "techrabble");
+  //$result = $mysqli->query("SELECT username FROM usertable");
+  /*
+  while ($row = $result->fetch_assoc()) {
+    echo $row['username']."<br>";
+  }
+  */
  ?>
+</body>
+</html>
