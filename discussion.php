fcde545b019da0758632cdc28c21a7d586366696
Param Ri <paramri@gatech.edu>
2018-07-01 01:41:17 -0400

    made recursive function for posting replies in comment section

--- discussion.php
+++ discussion.php
@@ -10,15 +10,15 @@
 	echo "<div class=\"container\">";
 	echo "<div class=\"jumbotron text-center\">";
 
-		$disc_id = $_GET['id']; 
-		$mysqli = new mysqli("localhost", "root", "HelloWorld2431@$", "techrabble");
-		$sql = "SELECT title, subj, body FROM discussions WHERE discId = " . $disc_id;
-		$result = $mysqli->query($sql);
-		$row = $result->fetch_assoc();
-		echo "<h1>" . $row['title'] . "</h1></div>";
-		echo "<div class=\"jumbotron text-center\">";
-		echo "<h2>" . $row['subj'] . "</h2>";
-		echo "<p>" . $row['body'] . "</p></div>";
+	$disc_id = $_GET['id']; 
+	$mysqli = new mysqli("localhost", "root", "HelloWorld2431@$", "techrabble");
+	$sql = "SELECT title, subj, body FROM discussions WHERE discId = " . $disc_id;
+	$result = $mysqli->query($sql);
+	$row = $result->fetch_assoc();
+	echo "<h1>" . $row['title'] . "</h1></div>";
+	echo "<div class=\"jumbotron text-center\">";
+	echo "<h2>" . $row['subj'] . "</h2>";
+	echo "<p>" . $row['body'] . "</p></div>";
 		
 	if(isset($_SESSION['signed_in']) && $_SESSION['signed_in'] == true) {
 		if($_SERVER['REQUEST_METHOD'] != 'POST') {
@@ -76,6 +76,21 @@
 						  $comment['body']
 						. ' </p>
 		';
+		postReplies($mysqli, $comment);
+		echo '</div>
+					  <p class="text-right"><a href="#" class="btn btn-default btn-sm"><i class="fa fa-reply"></i> reply</a></p>
+					</div>
+				  </div>
+				</div>
+			  </article>
+			  </section>
+		';
+	}
+	echo '</div>
+			</div>
+		</div>';
+		
+	function postReplies($mysqli, $comment) {
 		$sqlReplies = "SELECT replyPost FROM replys WHERE originalPost=". $comment['commentID'] . ";";
 		$resultReplies = $mysqli->query($sqlReplies);
 		$hasReplies = False;
@@ -104,6 +119,9 @@
 						<time class="comment-date" datetime="16-12-2014 01:05"><i class="fa fa-clock-o"></i> '. $resultReplies2['post_date'] .' </time>
 			';
 			echo '<p> '. $resultReplies2['body'].'</p>';
+			if ($resultReplies2 === True) {
+				postReplies($mysqli, $resultReplies2);
+			}
 			echo '
 				</div>
 			</div>
@@ -114,18 +132,7 @@
 			</div>
 		</div>';
 		}
-		echo '</div>
-					  <p class="text-right"><a href="#" class="btn btn-default btn-sm"><i class="fa fa-reply"></i> reply</a></p>
-					</div>
-				  </div>
-				</div>
-			  </article>
-			  </section>
-		';
 	}
-	echo '</div>
-			</div>
-		</div>';
 	?> 
   </div>
 </body>
